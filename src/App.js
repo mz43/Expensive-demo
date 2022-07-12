66469c2db2e2c629ec778300379455e2a276adbe
mz43 <109153156+mz43@users.noreply.github.com>
2022-07-12 18:03:33 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ const App = () => {
   return (
     <div>
       <h1>meysam</h1>
-    <h1>meysam</h1>
-    <h1>meysam</h1>
-    <h1>meysam</h1>
+    <h1>ali</h1>
+    <h1>amin</h1>
+    <h1>hossin</h1>
     </div>
   );
 };
